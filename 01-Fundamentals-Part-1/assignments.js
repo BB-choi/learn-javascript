e6005824cfbf13851b6a8c350961db2b01cda5e4
BB <choibobae0809@gmail.com>
2021-07-30 18:16:48 +0900

    LECTURE 10.  Values and Variables

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,7 @@
+let country = "Republic of korea";
+let continent = "Asia";
+let population = 51;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,22 @@
+let js = 'amzing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");//값
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let jonas_matilda = "JM"; // 특수문자 _,$만 사용가능
+let $function = 27; //예약어
+
+let person = 'jonas';//첫 대문자는 객체지향에서 사용
+let PI = 3.1415; // 변하지않는, 상수
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher'; // 좋은 예
+//descriptive
+
+
+let job1 = 'programmer';
+let job2 = 'teacher'; // 나쁜 예
