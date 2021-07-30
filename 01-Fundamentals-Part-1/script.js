12fdf68a2d131b746d0cff7bd671d66acb2f24d7
BB <choibobae0809@gmail.com>
2021-07-30 19:43:07 +0900

    LECTURE 12. Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,4 @@
-let js = 'amzing';
+/* let js = 'amzing';
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");//값
@@ -7,6 +7,7 @@ console.log(23);
 let firstName = "Jonas";
 console.log(firstName);
 
+// Variable name conventions
 let jonas_matilda = "JM"; // 특수문자 _,$만 사용가능
 let $function = 27; //예약어
 
@@ -20,3 +21,25 @@ let myCurrentJob = 'Teacher'; // 좋은 예
 
 let job1 = 'programmer';
 let job2 = 'teacher'; // 나쁜 예
+*/
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year); // 변수 year의 값도 undefined
+console.log(typeof year); // 변수 year의 type도 undefined
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); //object
