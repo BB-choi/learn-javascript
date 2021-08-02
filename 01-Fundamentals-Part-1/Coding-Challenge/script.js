2050be95fdf95635122a5739c1b9296e016ac87e
BB <choibobae0809@gmail.com>
2021-08-02 20:42:46 +0900

    LECTURE 25. Coding Challenge #3

--- 01-Fundamentals-Part-1/Coding-Challenge/script.js
+++ 01-Fundamentals-Part-1/Coding-Challenge/script.js
@@ -17,19 +17,19 @@ m tall.
 GOOD LUCK
  */
 
-let massMark = 78;
-let heightMark = 1.69;
-let massJohn = 92;
-let heightJohn = 1.95;
+// let massMark = 78;
+// let heightMark = 1.69;
+// let massJohn = 92;
+// let heightJohn = 1.95;
 
-massMark = 95;
-heightMark = 1.88;
-massJohn = 85;
-heightJohn = 1.76;
+// massMark = 95;
+// heightMark = 1.88;
+// massJohn = 85;
+// heightJohn = 1.76;
 
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / heightJohn ** 2;
-console.log(BMIMark, BMIJohn);
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / heightJohn ** 2;
+// console.log(BMIMark, BMIJohn);
 
 //const markHigherBMI = BMIMark > BMIJohn;
 //console.log(markHigherBMI);
@@ -45,9 +45,57 @@ is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark
 BMI (28.3) is higher than John's (23.9)!"
 Hint: Use an if/else statement
 GOOD LUCK
-*/
+
 if (BMIMark > BMIJohn) {
     console.log(`Mark's BMI (${BMIMark}) is higher than John's! (${BMIJohn}) `);
 } else {
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark}) !`);
-};
+}; */
+
+/*
+Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+GOOD LUCK
+*/
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas)
+//     console.log('Dolphins win the trophy 🏆');
+// else if (scoreKoalas > scoreDolphins)
+//     console.log('Koalas win the trophy 🏆');
+// else if (scoreDolphins === scoreKoalas)
+//     console.log('Both win the trophy 🏆');
+
+//Bonus1 & 2
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 106) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100)
+    console.log('Dolphins win the trophy 🏆');
+else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100)
+    console.log('Koalas win the trophy 🏆');
+else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100)
+    console.log('Both win the trophy 🏆');
+else
+    console.log('No one wins the trophy 😫');
