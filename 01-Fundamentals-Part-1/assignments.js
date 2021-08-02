48c18420ab326263605b5c37e08612417125d710
BB <choibobae0809@gmail.com>
2021-08-02 21:19:56 +0900

    LECTURE 26. The switch Statement

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -44,7 +44,7 @@ if (numNeibours === 1)
 else if (numNeibours > 1)
     console.log('More than 1 border');
 else
-    console.log('No borders'); */
+    console.log('No borders'); 
 
 const country = "Republic of korea";
 let population = 51;
@@ -54,4 +54,27 @@ const language = "Korean";
 if (language === "English" && population < 50 && !isIsland)
     console.log(`You should live in ${country} :)`)
 else
-    console.log(`${country} does not meet your criteria :(`)
+    console.log(`${country} does not meet your criteria :(`) */
+
+const language = "Korean";
+
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+}
