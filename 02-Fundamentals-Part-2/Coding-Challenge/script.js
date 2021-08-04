b94222f4bd864a49a760731d64edf454f0d60228
BB <choibobae0809@gmail.com>
2021-08-05 00:12:58 +0900

    LECTURE 41. Coding Challenge #2

--- 02-Fundamentals-Part-2/Coding-Challenge/script.js
+++ 02-Fundamentals-Part-2/Coding-Challenge/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Coding Challenge #1
 
 const calcAverage = (score1, score2, scrore3) => (score1 + score2 + scrore3) / 3;
@@ -27,4 +27,27 @@ scoreKoalas = calcAverage(23, 34, 27);
 
 console.log(scoreDolphins, scoreKoalas);
 
-checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas); */
+
+// Coding Challenge #2
+
+const calcTip = bill => (bill >= 50 && bill <= 300) ?
+    bill * 0.15 : bill * 0.2;
+// const calcTip = function(bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
+
+const bills = [125, 555, 44];
+const tips = [
+    calcTip(bills[0]),
+    calcTip(bills[1]),
+    calcTip(bills[2])
+];
+console.log(bills, tips);
+
+const totals = [
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2]
+];
+console.log(totals);
