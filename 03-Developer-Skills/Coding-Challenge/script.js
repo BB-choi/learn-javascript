5a88bdbb2e346dcae999e1d74b3d4a335b54f516
BB <choibobae0809@gmail.com>
2021-08-09 01:39:44 +0900

    LECTURE 62. Coding Challenge #1

---
+++ 03-Developer-Skills/Coding-Challenge/script.js
@@ -0,0 +1,31 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// 1) Understanding the problem
+// - Array transformed to stirng, separated by ...
+// - What is the X days? Answer : index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings needs to contain day (index+1)
+// - Add ... between elements and start and end of string
+// - Log string to console
+
+// § Data 1: [17, 21, 23]
+// § Data 2: [12, 5, -5, 0, 4]
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = '...';
+  for (let i = 0; i < arr.length; i++) {
+    str += ` ${arr[i]}ºC in ${i + 1}days ...`;
+  }
+  //   str += '...';
+  console.log(str);
+};
+
+printForecast(data1);
+printForecast(data2);
