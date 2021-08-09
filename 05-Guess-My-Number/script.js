86dbfb13427965f19bcd566d7cbb8e438652c646
BB <choibobae0809@gmail.com>
2021-08-09 15:04:53 +0900

    LECTURE 73. Handling Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,12 +1,23 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '✨ Correct Number!';
-// console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
-// console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  //   console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value); // Input field get a string
+  console.log(guess, typeof guess);
+  //   document.querySelector('.message').textContent = '✨ Correct Number!';
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '😯 No number!';
+  }
+});
