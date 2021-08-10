770537f4fea36ecc19d8b0e07ae26b9870daf12f
BB <choibobae0809@gmail.com>
2021-08-10 16:43:44 +0900

    LECTURE 80. Working With Classes

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,7 +5,26 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 // const btnsOpenModal = document.querySelector('.show-modal'); // Only first one will get selected
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+// console.log(btnsOpenModal);
+
+const openModal = function () {
+  // console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+
+  // modal.style.display = 'block';
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  //   console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+// NOT closeModal(), NOT call closeModal(), declare / define
+
+overlay.addEventListener('click', closeModal);
