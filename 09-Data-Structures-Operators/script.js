284508e6a91d97dcd915d3b898aa01af8669456d
BB <choibobae0809@gmail.com>
2021-08-14 17:15:21 +0900

    LECTURE 105. The Spread Operator (...)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -40,11 +40,65 @@ const restaurant = {
     // orderDelivery: function (obj) {
     // console.log(obj);
     console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}.`);
   },
 };
 
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+console.log(...str);
+// console.log('j', 'o');
+// console.log(`${...str} Schmedmann`);
+
+// Real-world exmaple
+const ingredients = [
+  // prompt("Let's make pasta! Ingredient 1?"),
+  // prompt('Ingredient 2?'),
+  // prompt('Ingredient 3?'),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
+
+// Objects from ES2018
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Risorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+/*
+// Desturcturing Objects
 restaurant.orderDelivery({
   time: '22:30',
   address: 'Via del Sole, 21',
@@ -67,7 +121,7 @@ const {
 } = restaurant;
 console.log(restaurantName, hours, tags);
 
-// Default vaules
+// Default values
 // restaurant.menu // undefined
 const { menu = [], starterMenu: starters = [] } = restaurant;
 console.log(menu, starters);
@@ -89,7 +143,7 @@ const {
 } = openingHours;
 console.log(o, c);
 
-/*
+
 // Destructuring Arrays
 const arr = [2, 3, 4];
 const a = arr[0];
