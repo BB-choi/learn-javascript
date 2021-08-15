0a26f32afcdf198cb085a6dd62c05f208596239c
BB <choibobae0809@gmail.com>
2021-08-15 17:50:02 +0900

    LECTURE 112. Optional Chaining (?.)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -59,6 +59,44 @@ const restaurant = {
   },
 };
 
+// console.log(restaurant.openingHours.mon); // undefined
+// console.log(restaurant.openingHours.mon.open); // Error undefined.open
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  // console.log(day);
+  // const open = restaurant.openingHours[day]?.open || 'closed'; // sat open at 0
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+  // openingHours.mon;
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [
+  {
+    name: 'Jonas',
+    email: 'hello@jonas.io',
+  },
+];
+// const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
+
 /*
 ///////////////////////////////////////
 // The for-of Loop
