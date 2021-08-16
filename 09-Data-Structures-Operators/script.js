624c2bc1ba52edaf1b4b7290adf95e77eac61489
BB <choibobae0809@gmail.com>
2021-08-16 12:07:39 +0900

    LECTURE 116. Maps: Fundamentals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -59,6 +59,44 @@ const restaurant = {
   },
 };
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Fireze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal')); // also return the updated map
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :)')
+  .set(false, 'We are closed :(');
+// rest.set(...) returns updated map
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+// rest.set([1, 2], 'Test');
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get([1, 2])); // undefined
+console.log(rest.get(arr));
+
+/*
+///////////////////////////////////////
+// Sets
+
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -98,7 +136,7 @@ console.log(
 
 console.log(new Set('jonasschmedtmann').size);
 
-/*
+
 ///////////////////////////////////////
 // Looping Objects : Object Keys, Values, and Entries
 
