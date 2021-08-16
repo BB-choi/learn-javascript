53551428f14c7ec04a2e7b5454f33dae8b6de0fe
BB <choibobae0809@gmail.com>
2021-08-16 21:09:09 +0900

    LECTURE 128. How Passing Arguments Works: Value vs. Reference

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 
 // const createBooking = function (
@@ -33,3 +34,36 @@ createBooking('LH123', 5);
 
 createBooking('LH123', 1000); // can't skip defalut parameter(numPassengers)
 createBooking('LH123', undefined, 1000);
+*/
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 232131231231,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 232131231231) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight); //LH234
+// console.log(jonas); // {name: "Mr. Jonas Schmedtmann", passport: 232131231231}
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas; // copy only refernce
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
