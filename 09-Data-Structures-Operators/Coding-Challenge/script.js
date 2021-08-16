8e9ce54f1ce254fe8a860ea92844562b6c4b2b19
BB <choibobae0809@gmail.com>
2021-08-16 17:17:52 +0900

    LECTURE 123. Coding Challenge #4

--- 09-Data-Structures-Operators/Coding-Challenge/script.js
+++ 09-Data-Structures-Operators/Coding-Challenge/script.js
@@ -130,7 +130,7 @@ for (const player of game.scored) {
   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 }
 console.log(scorers);
-*/
+
 
 // Coding Challenge #3
 
@@ -179,3 +179,27 @@ for (const [min, event] of gameEvents) {
   const half = min <= 45 ? 'FIRST' : 'SECOND';
   console.log(`[${half} HALF] ${min}: ${event}`);
 }
+*/
+
+// Coding Challenge #4
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+
+    // console.log(output.padEnd(20, ' '));
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
