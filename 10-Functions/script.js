069212dd8483b0026b4c175c850dfb860b6161c5
BB <choibobae0809@gmail.com>
2021-08-16 22:15:33 +0900

    LECTURE 131. Functions Returning Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -67,7 +67,7 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
-*/
+
 
 const oneWord = function (str) {
   return str.replace(/ /g, '').toLowerCase();
@@ -96,3 +96,20 @@ const high5 = function () {
 document.body.addEventListener('click', high5);
 
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
+*/
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+// Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+greetArr('Hi!')('Jonas');
