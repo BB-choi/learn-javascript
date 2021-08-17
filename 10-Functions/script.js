e5a4039ae1bf8798288be8c41c827cfcbcc25dfb
BB <choibobae0809@gmail.com>
2021-08-17 23:54:37 +0900

    LECTURE 137. More Closure Examples

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -244,7 +244,7 @@ runOnce();
 }
 // console.log(isPrivate); // CAN'T access
 console.log(notPrivate); // accessible
-*/
+
 
 const secureBooking = function () {
   let passengerCount = 0;
@@ -262,3 +262,51 @@ booker(); // 2 passengers
 booker(); // 3 passengers
 
 console.dir(booker);
+*/
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// setTimeout(function () {
+//   console.log('TIMER');
+// }, 1000); // millisecond
+
+// closure have priority over the scope chain
+const perGroup = 1000;
+
+boardPassengers(180, 3);
