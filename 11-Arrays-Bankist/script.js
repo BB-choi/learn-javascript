cf39c28a95282d844de44daeab474af60638a66c
BB <choibobae0809@gmail.com>
2021-08-20 22:54:43 +0900

    LECTURE 155. The find Method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -325,7 +325,7 @@ const max = movements.reduce((acc, mov) => {
 }, movements[0]);
 
 console.log(max);
-*/
+
 
 const eurToUsd = 1.1;
 console.log(movements);
@@ -345,3 +345,13 @@ const totalDepositUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(totalDepositUSD);
+*/
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
