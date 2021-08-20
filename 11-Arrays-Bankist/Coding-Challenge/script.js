7991ee69ebf7ef4580c7504497b2a1341222f6a1
BB <choibobae0809@gmail.com>
2021-08-20 22:39:25 +0900

    LECTURE 154. Coding Challenge #3

--- 11-Arrays-Bankist/Coding-Challenge/script.js
+++ 11-Arrays-Bankist/Coding-Challenge/script.js
@@ -114,7 +114,7 @@ const checkDogs = function (dogsJulia, dogsKate) {
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
-*/
+
 
 /////////////////////////////////////////////////
 // Coding Challenge #2
@@ -136,6 +136,21 @@ const calcAverageHumanAge = function (ages) {
 
   return average;
 };
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
+*/
+
+/////////////////////////////////////////////////
+// Coding Challenge #3
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
