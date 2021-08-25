d47b6a095c5917f96e0e9c3bf9e1c81fef813334
BB <choibobae0809@gmail.com>
2021-08-26 08:22:41 +0900

    LECTURE 184. Styles, Attributes and Classes

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -73,3 +73,52 @@ document
     message.remove();
     // message.parentElement.removeChild(message);
   });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+// console.log(message.style.height);
+// console.log(message.style.color);
+console.log(message.style.backgroundColor); // only works for inline styles
+
+// console.log(getComputedStyle(message));
+console.log(getComputedStyle(message).color); // "rgb(187, 187, 187)"
+console.log(getComputedStyle(message).height); // "50px"
+
+message.style.height =
+  // Number.parseInt(getComputedStyle(message).height, 10) + 40 + 'px';
+  Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer); // undefined
+console.log(logo.getAttribute('designer')); // 'Jonas'
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src); // absolute
+console.log(logo.getAttribute('src')); // 'img/logo.png'. relative
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href')); // '#'
+
+// Data attributes
+console.log(logo.dataset.versionNumber); // data-version-number="3.0"
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// Don't use
+logo.className = 'jonas';
