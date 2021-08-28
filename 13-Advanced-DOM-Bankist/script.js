3669e3f768c44b7133e153414a05f531fd5039c4
BB <choibobae0809@gmail.com>
2021-08-28 23:04:39 +0900

    LECTURE 199. Lifecycle DOM Events

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -559,3 +559,20 @@ console.log(h1.parentElement.children);
   }
 });
 */
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+// document.ready; // jQuery
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+//   // e.returnValue = 'message'; // not available
+//   // return false;
+// });
