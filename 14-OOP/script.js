9e2767cca0c5a398b4562c75d64b75f592b9cfba
BB <choibobae0809@gmail.com>
2021-08-29 06:12:54 +0900

    LECTURE 208. Prototypal Inheritance on Built-In Objects

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -55,3 +55,28 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName')); // true
 console.log(jonas.hasOwnProperty('species')); // false
+
+console.log(jonas.__proto__);
+// Object.protype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__); // null
+
+console.log(Person.prototype.constructor);
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 4, 3, 1, 6, 5]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype); // true
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1); // __proto__ : HTMLHeadingElement, HTMLElement, Element, Node, EventTarget, Object
+
+console.dir(x => x + 1);
