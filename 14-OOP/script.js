99723172f82b513cdef8993db09264d189dce834
BB <choibobae0809@gmail.com>
2021-08-29 02:31:43 +0900

    LECTURE 206. Prototypes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -29,3 +29,29 @@ const jay = 'Jay';
 
 console.log(jonas instanceof Person); // true
 console.log(jay instanceof Person); // false
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype); // true
+
+console.log(Person.prototype.isPrototypeOf(jonas)); // true
+console.log(Person.prototype.isPrototypeOf(matilda)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas, matilda);
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName')); // true
+console.log(jonas.hasOwnProperty('species')); // false
