96313f84cf101863eb64ee6e94872854743e6b66
BB <choibobae0809@gmail.com>
2021-08-29 21:40:17 +0900

    LECTURE 213. Object.create

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -178,3 +178,27 @@ const jonas = new Person('Jonas', 1991);
 
 Person.hey();
 // jonas.hey(); // error.
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven); // {}
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge(); // 35
+
+console.log(steven.__proto__);
+console.log(steven.__proto__ === PersonProto); // true
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge(); // 58
