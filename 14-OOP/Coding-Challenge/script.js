1faabf3b64197e84f7ca6a40914bd5f93022a2cb
BB <choibobae0809@gmail.com>
2021-08-29 23:23:18 +0900

    LECTURE 216. Coding Challenge #3

--- 14-OOP/Coding-Challenge/script.js
+++ 14-OOP/Coding-Challenge/script.js
@@ -27,7 +27,7 @@ bmw.accelerate();
 
 mercedes.accelerate();
 mercedes.brake();
-*/
+
 
 class CarCl {
   constructor(make, speed) {
@@ -62,3 +62,49 @@ ford.brake();
 
 ford.speedUS = 50;
 console.log(ford);
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// Link the prototypes
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  // this.charge -= 1;
+  this.charge--;
+
+  console.log(
+    `${this.make} going at ${this.speed}km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake();
+tesla.accelerate();
