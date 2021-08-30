f0701cbc132bd0973f642a55ad98c16cc7d49d56
BB <choibobae0809@gmail.com>
2021-08-30 14:36:51 +0900

    LECTURE 218. Inheritance Between "Classes": Object.create

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -255,7 +255,7 @@ console.log(mike.__proto__ === Person.prototype); // false
 
 console.log(Student.prototype.__proto__ === Person.prototype); // true
 console.log(Person.prototype.__proto__ === Object.prototype); // true
-*/
+
 
 ///////////////////////////////////////
 // Inheritance Between "Classes": ES6 Classes
@@ -324,3 +324,35 @@ class StudentCl extends PersonCl {
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
+*/
+
+///////////////////////////////////////
+// Inheritance Between "Classes": Object.Create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.fullName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
