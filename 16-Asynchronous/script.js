8a32680f8553405c42c08a7ad5cf448aca8ed77c
BB <choibobae0809@gmail.com>
2021-09-03 16:35:22 +0900

    LECTURE 249. Chaining Promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -135,9 +135,28 @@ console.log(request);
 // };
 
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+      // return 23; // 23 will become a promise value
+
+      //  DON'T DO THIS
+      //   fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`).then(
+      //     response => response.json()
+      //   );
+    })
+    // .then(data => console.log(data)); // data is fullfilled value
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('portugal');
+// getCountryData('germany');
