c270bfbb48126a61cdbf07c7d8fc06ea43e4a13e
BB <choibobae0809@gmail.com>
2021-09-04 18:50:52 +0900

    LECTURE 254. The Event Loop in Practice

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -124,6 +124,7 @@ setTimeout(() => {
 }, 1000);
 */
 
+/*
 // const request = fetch('https://restcountries.eu/rest/v2/name/portugal');
 // console.log(request);
 
@@ -219,3 +220,16 @@ btn.addEventListener('click', function () {
 });
 
 getCountryData('australia');
+*/
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
