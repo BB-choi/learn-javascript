b154d4f193a9a3688dc7f5f96d461a8052f3022c
BB <choibobae0809@gmail.com>
2021-09-05 19:15:58 +0900

    LECTURE 259. Error Handling With try...catch

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -28,7 +28,7 @@ const renderCountry = function (data, className = '') {
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
-  //   countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 ///////////////////////////////////////
@@ -356,30 +356,41 @@ const getPosition = function () {
 };
 
 const whereAmI = async function (country) {
-  // Geolocation
-  const pos = await getPosition();
-  const { latitude: lat, longitude: lng } = pos.coords;
+  try {
+    // Geolocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
-  // Reverse geocoding
-  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?json=1`);
-  const dataGeo = await resGeo.json();
-  console.log(dataGeo);
+    // Reverse geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?json=1`);
+    if (!resGeo.ok) throw new Error('Problem getting location data');
 
-  // Country data
-  // const res = await fetch(
-  //   `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
-  // );
-  const res = await fetch(
-    `https://restcountries.eu/rest/v2/alpha/${dataGeo.prov}`
-  );
-  // console.log(res);
+    const dataGeo = await resGeo.json();
+    console.log(dataGeo);
 
-  const data = await res.json();
-  console.log(data);
-  // renderCountry(data[0]);
-  renderCountry(data);
+    // Country data
+    const res = await fetch(
+      `https://restcountries.eu/rest/v2/alpha/${dataGeo.prov}`
+    );
+    if (!res.ok) throw new Error('Problem getting country');
+
+    const data = await res.json();
+    console.log(data);
+    renderCountry(data);
+  } catch (err) {
+    console.error(`${err} ❌`);
+    renderError(`❌ (${err.message})`);
+  }
 };
 
-// whereAmI('portugal');
 whereAmI();
 console.log('FIRST');
+
+// try {
+//   let y = 1;
+//   const x = 2;
+//   // x = 3;
+//   y = 3;
+// } catch (err) {
+//   alert(err.message);
+// }
