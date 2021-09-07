855ba6a5f055f2c5b938343d3ac4a4c94c5237df
BB <choibobae0809@gmail.com>
2021-09-07 13:04:47 +0900

    LECTURE 269. The Module Pattern

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -1,3 +1,4 @@
+/*
 // Importing module
 
 // import {
@@ -27,3 +28,34 @@ add('bread', 5);
 add('apples', 2);
 
 console.log(cart);
+*/
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost  is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2); // private
+console.log(ShoppingCart2.shippingCost); // undefined
