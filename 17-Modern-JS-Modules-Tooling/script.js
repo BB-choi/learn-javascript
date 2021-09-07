81050938eff537dfc3962b0c5284e7dbc8884415
BB <choibobae0809@gmail.com>
2021-09-07 12:50:32 +0900

    LECTURE 268. Exporting and Importing in ES6 Modules

---
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -0,0 +1,29 @@
+// Importing module
+
+// import {
+//   addToCart,
+//   totalPrice as price,
+//   //   totalQuantity,
+//   tq,
+// } from './shoppingCart.js';
+// addToCart('bread', 5);
+// // console.log(totalPrice, totalQuantity);
+// console.log(price, tq);
+
+console.log('Importing module');
+// console.log(shippingCost);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+// import add from './shoppingCart.js';
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 2);
+
+console.log(cart);
---
+++ 17-Modern-JS-Modules-Tooling/shoppingCart.js
@@ -0,0 +1,24 @@
+// Exporting module
+console.log('Exporting module');
+
+const shippingCost = 10;
+export const cart = [];
+
+// if(true){
+//     export
+// }
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
