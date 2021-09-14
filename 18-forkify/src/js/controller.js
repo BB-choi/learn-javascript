fc09e544bba122cd52853732ffef42ed2a009079
BB <choibobae0809@gmail.com>
2021-09-14 09:39:28 +0900

    LECTURE 292. Implementing Pagination - Part 1

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -8,9 +8,9 @@ import 'regenerator-runtime/runtime';
 import recipeView from './views/recipeView.js';
 import { async } from 'regenerator-runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -43,7 +43,8 @@ const controlSearhResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) {
     console.log(err);
   }
