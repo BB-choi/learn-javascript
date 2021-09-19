748cf18b26dec748d7859f4f8a278f1f40b69911
BB <choibobae0809@gmail.com>
2021-09-19 14:12:58 +0900

    LECTURE 302. Uploading a New Recipe - Part 2-1 Create object to be uploaded

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -94,10 +94,16 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
-const controlAddRecipe = function (newRecipe) {
-  console.log(newRecipe);
+const controlAddRecipe = async function (newRecipe) {
+  // console.log(newRecipe);
 
-  // Upload the new recipe data
+  try {
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+  } catch (err) {
+    console.error('✋', err);
+    addRecipeView.renderError(err.message);
+  }
 };
 
 const init = function () {
